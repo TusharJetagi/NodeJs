7adc6f00ff35fe5369a4fcaa7be4ede5fd9dc9e0
Tushar Jetagi <tusharjetagi87@gmail.com>
2025-06-25 15:24:29 +0530

    shifted to mongodb atlas

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 //define the mongodb connection url
-const mongoURL = "mongodb://localhost:27017/hotels";
+const mongoURL = process.env.DB_URL;
 
 // set up mongodb connection
 mongoose.connect(mongoURL, {
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
@@ -11,6 +15,6 @@ app.use("/person", personRoutes);
 app.use("/menu", menuItemRoutes);
 
 // start the server
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Listening on port 3000");
 });
